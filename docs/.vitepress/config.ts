edd25e4338baf7a99f0805663daa00a4cee98afc
azzmb <usagi_122@proton.me>
2026-01-29 14:21:30 +0800

    Update config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,23 +1,22 @@
 import { defineConfig } from 'vitepress';
 
 export default defineConfig({
-  base: '/linkerx/',
+  base: '/',
   lang: 'zh-CN',
   title: 'LinkerX',
   description: 'LinkerX 基于 gost 的转发面板',
 
   themeConfig: {
     nav: [
-      { text: '文档', link: '/getting-started/' },
-      { text: '使用指南', link: '/guide/' },
+      { text: '文档', link: '/getting-started' },
+      { text: '使用指南', link: '/guide' },
     ],
-
     sidebar: [
       {
         text: '指南',
         items: [
-          { text: '文档', link: '/getting-started/' },
-          { text: '使用指南', link: '/guide/' },
+          { text: '文档', link: '/getting-started' },
+          { text: '使用指南', link: '/guide' },
         ],
       },
     ],
